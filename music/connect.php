09c97ad051a42b34b0fc5435aee95ab857639915
longup113 <nightowl0202@gmail.com>
2021-06-09 15:22:12 +0700

    daedit

--- music/connect.php
+++ music/connect.php
@@ -1,8 +1,8 @@
 <?php
-		$hostname = "localhost";
-		$user = "root";
-		$pass = "";
-		$db = "music";
+		$hostname = "34.206.113.91";
+		$user = "thanh";
+		$pass = "thanh@123";
+		$db = "no1sl";
 
 		$con = mysqli_connect($hostname,$user,$pass,$db);
 		mysqli_query($con,$db);
